79af2561d3755d80b50884a1ab494e61cddde83e
duduBmoon21 <eleanortefera21@gmail.com>
2024-09-25 22:07:18 -0700

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for a simple interaction on the play button
+document.getElementById('playVideo').addEventListener('click', function() {
+    alert('Video Play Feature Coming Soon!');
+});
